5ef2998ef8c5c56db6ddb85f47a8d13d2dc21821
David Kochkin <kochkin.david@gmail.com>
2024-02-10 13:37:19 +0200

    Initial Commit

---
+++ tipCalculator/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  tipCalculator
+//
+//  Created by David Kochkin on 10.02.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = ViewController()
+        window.rootViewController = vc
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by David Kochkin on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
