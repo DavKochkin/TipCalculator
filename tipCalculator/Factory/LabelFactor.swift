9d3d8dc81b4d59b912f570c7f0146cf60e4ca99a
David Kochkin <kochkin.david@gmail.com>
2024-02-11 11:39:52 +0200

    Setup ResultView

--- tipCalculator/Factory/LabelFactor.swift
+++ tipCalculator/Factory/LabelFactor.swift
@@ -15,12 +15,12 @@ struct LabelFactory {
         backgroundColor: UIColor = .clear,
         textColor: UIColor = ThemeColor.text,
         textAlignment: NSTextAlignment = .center) -> UILabel {
-            let label = UILabel()
-            label.text = text
-            label.font = font
+            let label             = UILabel()
+            label.text            = text
+            label.font            = font
             label.backgroundColor = backgroundColor
-            label.textColor = textColor
-            label.textAlignment = textAlignment
+            label.textColor       = textColor
+            label.textAlignment   = textAlignment
             return label
         }
 }
